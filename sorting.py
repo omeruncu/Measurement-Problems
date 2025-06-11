59f07150a7774a90fd4343a80c78244f4ba556ed
omeru <omeruncu@outlook.com>
2025-06-11 14:35:32 +0300

    Sorting studies has been done.

--- sorting.py
+++ sorting.py
@@ -11,30 +11,31 @@ pd.set_option('display.float_format', lambda x: '%.5f' % x)
 # Course Ranking
 ###################################################
 
-df = pd.read_csv("datasets/product_sorting.csv")
+df_ = pd.read_csv("datasets/product_sorting.csv")
+df = df_
 print(df.shape)
 df.head(10)
 
 ## Sorting by Rating
-df.sort_values("rating", ascending=False).head(20)
+df.sort_values("rating", ascending = False).head(20)
 
 ## Sorting by Comment Count or Purchase Count
-df.sort_values("purchase_count", ascending=False).head(20)
-df.sort_values("commment_count", ascending=False).head(20)
+df.sort_values("purchase_count", ascending = False).head(20)
+df.sort_values("commment_count", ascending = False).head(20)
 
 ## Sorting by Rating, Comment and Purchase
 
-df["purchase_count_scaled"] = MinMaxScaler(feature_range=(1, 5)). \
+df["purchase_count_scaled"] = MinMaxScaler(feature_range = (1, 5)). \
     fit(df[["purchase_count"]]). \
     transform(df[["purchase_count"]])
 
 df.describe().T
 
-df["comment_count_scaled"] = MinMaxScaler(feature_range=(1, 5)). \
+df["comment_count_scaled"] = MinMaxScaler(feature_range = (1, 5)). \
     fit(df[["commment_count"]]). \
     transform(df[["commment_count"]])
 
-df["comment_count_scaled"] = MinMaxScaler(feature_range=(1, 5)). \
+df["comment_count_scaled"] = MinMaxScaler(feature_range = (1, 5)). \
     fit(df[["commment_count"]]). \
     transform(df[["commment_count"]])
 
@@ -45,6 +46,180 @@ def weighted_sorting_score(dataframe, w1=32, w2=26, w3=42):
 
 df["weighted_sorting_score"] = weighted_sorting_score(df)
 
-df.sort_values("weighted_sorting_score", ascending=False).head(20)
+df.sort_values("weighted_sorting_score", ascending = False).head(20)
 
-df[df["course_name"].str.contains("Veri Bilimi")].sort_values("weighted_sorting_score", ascending=False).head(20)
+df[df["course_name"].str.contains("Veri Bilimi")].sort_values("weighted_sorting_score", ascending = False).head(20)
+
+## Bayesian Average Rating Score
+### Sorting Products with 5 Star Rated
+### Sorting Products According to Distribution of 5 Star Rating
+
+
+def bayesian_average_rating(n, confidence=0.95):
+    if sum(n) == 0:
+        return 0
+    K = len(n)
+    z = st.norm.ppf(1 - (1 - confidence) / 2)
+    N = sum(n)
+    first_part = 0.0
+    second_part = 0.0
+    for k, n_k in enumerate(n):
+        first_part += (k + 1) * (n[k] + 1) / (N + K)
+        second_part += (k + 1) * (k + 1) * (n[k] + 1) / (N + K)
+    score = first_part - z * math.sqrt((second_part - first_part * first_part) / (N + K + 1))
+    return score
+
+
+df.head()
+
+df["bar_score"] = df.apply(lambda x: bayesian_average_rating(x[["1_point",
+                                                                "2_point",
+                                                                "3_point",
+                                                                "4_point",
+                                                                "5_point"]]), axis = 1)
+
+df.sort_values("weighted_sorting_score", ascending = False).head(20)
+df.sort_values("bar_score", ascending = False).head(20)
+
+df[df["course_name"].index.isin([5, 1])].sort_values("bar_score", ascending = False)
+
+## Hybrid Sorting: BAR Score + Other Factors
+
+
+def hybrid_sorting_score(dataframe, bar_w=60, wss_w=40):
+    bar_score = dataframe.apply(lambda x: bayesian_average_rating(x[["1_point",
+                                                                     "2_point",
+                                                                     "3_point",
+                                                                     "4_point",
+                                                                     "5_point"]]), axis = 1)
+    wss_score = weighted_sorting_score(dataframe)
+
+    return bar_score*bar_w/100 + wss_score*wss_w/100
+
+
+df["hybrid_sorting_score"] = hybrid_sorting_score(df)
+
+df.sort_values("hybrid_sorting_score", ascending = False).head(20)
+
+df[df["course_name"].str.contains("Veri Bilimi")].sort_values("hybrid_sorting_score", ascending = False).head(20)
+
+
+## APPLICATION: IMDB Movie Scoring & Sorting
+
+df_ = pd.read_csv("datasets/movies_metadata.csv",
+                 low_memory = False)
+
+df = df_
+
+df = df[["title", "vote_average", "vote_count"]]
+
+df.head()
+df.shape
+
+### Sort by Vote Average
+
+df.sort_values("vote_average", ascending = False).head(20)
+
+df["vote_count"].describe([0.10, 0.25, 0.50, 0.70, 0.80, 0.90, 0.95, 0.99]).T
+
+df[df["vote_count"] > 400].sort_values("vote_average", ascending = False).head(20)
+
+df["vote_count_score"] = MinMaxScaler(feature_range = (1, 10)). \
+    fit(df[["vote_count"]]). \
+    transform(df[["vote_count"]])
+
+### # vote_average * vote_count
+
+df["average_count_score"] = df["vote_average"] * df["vote_count_score"]
+
+df.sort_values("average_count_score", ascending=False).head(20)
+
+### IMDB Weighted Rating
+
+#### weighted_rating = (v/(v+M) * r) + (M/(v+M) * C)
+
+#### r = vote average
+#### v = vote count
+#### M = minimum votes required to be listed in the Top 250
+#### C = the mean vote across the whole report (currently 7.0)
+
+#### Film 1:
+#### r = 8
+#### M = 500
+#### v = 1000
+
+#### Part one:
+#### (1000 / (1000+500)) * 8 = 5.33
+
+#### Part two:
+#### 500/(1000+500) * 7 = 2.33
+
+#### Total = 5.33 + 2.33 = 7.66
+
+
+#### Film 2:
+#### r = 8
+#### M = 500
+#### v = 3000
+
+#### Part one:
+#### (3000 / (3000+500)) * 8 = 6.85
+
+#### Part two:
+#### 500/(3000+500) * 7 = 1
+
+#### Total = 7.85
+
+M = 2500
+C = df['vote_average'].mean()
+
+def weighted_rating(r, v, M, C):
+    return (v / (v + M) * r) + (M / (v + M) * C)
+
+df.sort_values("average_count_score", ascending=False).head(10)
+
+#### Deadpool
+weighted_rating(7.40000, 11444.00000, M, C)
+#### Inception
+weighted_rating(8.10000, 14075.00000, M, C)
+#### The Shawshank Redemption
+weighted_rating(8.50000, 8358.00000, M, C)
+
+df["weighted_rating"] = weighted_rating(df["vote_average"],
+                                        df["vote_count"], M, C)
+
+df.sort_values("weighted_rating", ascending=False).head(10)
+
+### Bayesian Average Rating Score
+#### 12481                                    The Dark Knight
+#### 314                             The Shawshank Redemption
+#### 2843                                          Fight Club
+#### 15480                                          Inception
+#### 292                                         Pulp Fiction
+
+#### The Shawshank Redemption
+bayesian_average_rating([34733, 4355, 4704, 6561, 13515, 26183, 87368, 273082, 600260, 1295351])
+#### The Godfather
+bayesian_average_rating([37128, 5879, 6268, 8419, 16603, 30016, 78538, 199430, 402518, 837905])
+
+df_ = pd.read_csv("datasets/imdb_ratings.csv")
+df = df_
+df = df.iloc[0:, 1:]
+
+
+df["bar_score"] = df.apply(lambda x: bayesian_average_rating(x[["one", "two", "three", "four", "five",
+                                                                "six", "seven", "eight", "nine", "ten"]]), axis=1)
+df.sort_values("bar_score", ascending=False).head(20)
+
+
+# Weighted Average Ratings
+# IMDb publishes weighted vote averages rather than raw data averages.
+# The simplest way to explain it is that although we accept and consider all votes received by users,
+# not all votes have the same impact (or ‘weight’) on the final rating.
+
+# When unusual voting activity is detected,
+# an alternate weighting calculation may be applied in order to preserve the reliability of our system.
+# To ensure that our rating mechanism remains effective,
+# we do not disclose the exact method used to generate the rating.
+#
+# See also the complete FAQ for IMDb ratings.
